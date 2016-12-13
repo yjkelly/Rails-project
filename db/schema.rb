524a28b3724a0be2a7ecde259321a3b5f1234a39
William Lacey <wlacey@gmail.com>
2016-12-13 19:03:30 +0000

    no change

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161213152907) do
+ActiveRecord::Schema.define(version: 20161212174604) do
 
   create_table "accommodations", force: :cascade do |t|
     t.string   "name",        limit: 100,                  null: false
